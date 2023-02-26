7688608a0b8390a05347d0fc85c05b399d779c3e
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-02-26 11:41:37 -0700

    Adding my css resusable classes

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,96 @@
 </template>
 
 <style lang="scss">
+@import '@/scss/variables.scss';
+
 * {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
 }
 
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: $--header-height 0 0 0;
+  font-family: $--body-font;
+  font-size: $--normal-font-size;
+  background-color: $--body-color;
+  color: $--text-color;
+}
+
+h1, h2, h3 {
+  color: $--title-color;
+  font-weight: $--font-semi-bold;
+  font-family: $--title-font;
+}
+
+ul {
+  list-style: none;
+}
+
 a {
-  &.router-link-exact-active {
-    color: #42b983;
+  text-decoration: none;
+//   &.router-link-exact-active {
+//     color: #42b983;
+//   }
+}
+
+img, video {
+  max-width: 100%;
+  height: auto;
+}
+
+button, input {
+  border: none;
+  font-family: $--body-font;
+  font-size: $--normal-font-size;
+}
+
+button {
+  cursor: pointer;
+}
+
+input {
+  outline: none;
+}
+
+/* Reusable classes */
+.section {
+  padding: 4.5rem 0 2.5rem;
+}
+
+.section__title {
+  font-size: $--h2-font-size;
+  color: $--title-color;
+  text-align: center;
+  text-transform: capitalize;
+  margin-bottom: $--mb-2;
+}
+
+.container {
+  max-width: 968px;
+  margin-left: $--mb-1;
+  margin-right: $--mb-1;
+}
+
+.grid {
+  display: grid;
+  gap: 1.5rem;
+}
+
+
+@media screen and (min-width: 968px) {
+  :root {
+    --biggest-font-size: 4rem;
+    --h1-font-size: 2.25rem;
+    --h2-font-size: 1.75rem;
+    --h3-font-size: 1.25rem;
+    --normal-font-size: 1rem;
+    --small-font-size: .875rem;
+    --smaller-font-size: .813rem;
   }
 }
 </style>
