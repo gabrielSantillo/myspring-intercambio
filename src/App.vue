c818a24fc8d4482856ad24831705a756f8b4b16c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-02-26 11:20:45 -0700

    Starting the project

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
 }
-
-nav {
-  padding: 30px;
 
   a {
     font-weight: bold;
@@ -28,5 +17,4 @@ nav {
       color: #42b983;
     }
   }
-}
 </style>
