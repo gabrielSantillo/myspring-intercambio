897536f6ddf3acdb3da26408cdb26d6f94f547ff
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-02-28 18:54:49 -0700

    Added the home tags

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,6 +1,33 @@
 <template>
     <div class="home">
         <img src="@/img/home1.jpg" alt="" class="home__img">
+
+        <div class="home__container container grid">
+             <div class="home__data">
+                <span class="home__data-subtitle">Discover your place</span>
+                <h1 class="home__data-title">Explore The <br> Best Destinations</h1>
+                <a href="#" class="button">Explore</a>
+             </div>
+
+            <div class="home__social">
+                <a href="#" target="_blank"  class="home__social-link"><i class="ri-facebook-circle-fill"></i></a>
+                <a href="#" target="_blank" class="home__social-link"><i class="ri-instagram-fill"></i></a>
+                <a href="#" target="_blank" class="home__social-link"><i class="ri-youtube-fill"></i></a>
+            </div>
+
+            <div class="home__info">
+                <div>
+                    <span class="home__info-title">5 best places to visit</span>
+                    <a href="#" class="button button--flex button--link home__info-button">
+                        More <i class="ri-arrow-right-line"></i>
+                    </a>
+                </div>
+
+                <div class="home__info-overlay">
+                    <img src="@/img/home2.jpg" alt="" class="home__info-img">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
