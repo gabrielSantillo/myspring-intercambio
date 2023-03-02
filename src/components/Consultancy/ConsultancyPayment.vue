c8bbda036ff7b73dc1f7918da0d9b1430a185149
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-02 15:51:26 -0700

    Added one more component to the consultancy view

---
+++ src/components/Consultancy/ConsultancyPayment.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="section">
+    <h2 class="section__title">Pagamento</h2>
+    <div class="plan__container container">
+        <p class="plan__description">
+        Serviço de consultoria <br>
+        Contrato 12 meses
+        </p>
+        <p class="plan__description">
+        CAD$ 200.00 <br>
+        (PayPal, Wise ou Pix)
+        </p>
+        <p class="plan__description">
+        Solicite sua invoice por email <br>
+        Assinatura do contrato de prestação de serviçoes via Autentique
+        </p>
+        <a href="#" class="button">Solicitar invoice</a>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.plan__container {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    row-gap: 1rem;
+}
+
+.button {
+    display: inline-block;
+    background-color: $--first-color;
+    color: $--white-color;
+    padding: 1rem 2rem;
+    font-weight: $--font-semi-bold;
+    transition: .3s;
+    border-radius: .5rem;
+    width: 180px;
+    margin: auto;
+    margin-top: 1rem;
+}
+
+.button:hover {
+    box-shadow: 4px 8px 8px hsla(157, 12%, 90%, 1);
+}
+</style>
--- src/views/ConsultancyView.vue
+++ src/views/ConsultancyView.vue
@@ -4,6 +4,7 @@
         <consultancy-planning-component></consultancy-planning-component>
         <consultancy-time-line-component></consultancy-time-line-component>
         <consultancy-bullets-component></consultancy-bullets-component>
+        <consultancy-payment></consultancy-payment>
     </div>
 </template>
 
@@ -12,8 +13,9 @@ import ConsultancyComponent from '@/components/Consultancy/ConsultancyComponent.
 import ConsultancyPlanningComponent from '@/components/Consultancy/ConsultancyPlanComponent.vue'
 import ConsultancyTimeLineComponent from '@/components/Consultancy/ConsultancyTimeLineComponent.vue'
 import ConsultancyBulletsComponent from '@/components/Consultancy/ConsultancyBulletsComponent.vue'
+import ConsultancyPayment from '@/components/Consultancy/ConsultancyPayment.vue'
     export default {
-  components: { ConsultancyComponent, ConsultancyPlanningComponent, ConsultancyTimeLineComponent, ConsultancyBulletsComponent },
+  components: { ConsultancyComponent, ConsultancyPlanningComponent, ConsultancyTimeLineComponent, ConsultancyBulletsComponent, ConsultancyPayment },
         
     }
 </script>
