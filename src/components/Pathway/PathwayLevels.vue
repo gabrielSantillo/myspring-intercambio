88b99685828105a3a1dd9039151105c7394b0fce
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-02 19:41:31 -0700

    Adding one more component to the pathway view

---
+++ src/components/Pathway/PathwayLevels.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="pathway__level section">
+        <div class="pathway__level-leveling">
+            <h2 class="section__title">
+                Como funciona o nivelamento das turmas?
+            </h2>
+            <div class="pathway__level-leveling-description container">
+
+                <p>Todos os alunos fazem um teste antes de iniciar suas aulas, e o resultado serve para nivelar esse estudante em uma turma de acordo com seu nível acadêmico. Os alunos de nível básico e intermediário  iniciam obrigatoriamente nas turmas de General English, e quando atingem o nível avançado (Level 10), podem optar pelo Academic English ou o Preparatório de IELTS</p>
+
+                <p>GENERAL ENGLISH:  nível básico ao proficiente (Level 1 a 17)</p>
+
+                <p>ACADEMIC ENGLISH: nível básico ao proficiente (Level 10 a 17 = Pw1 a Pw3)</p>
+
+                <p>PREPARATÓRIO IELTS: nível básico ao proficiente (Level 10 a 17)</p>
+            </div>
+        </div>
+
+        <div class="pathway__level-img">
+            <img src="@/img/place5.jpg" alt="">
+        </div>
+
+        <div class="pathway__level-duration">
+            <h2 class="section__title">Quanto tempo dura cada nível do curso?</h2>
+            <div class="pathway__level-duration-description">
+                <p>Cada English Band tem duração mínima de 4 semanas. Ao final da quarta semana o aluno faz uma avaliação para testar sua evolução, obtendo a nota exigida ele automaticamente passa para o próximo nível. Caso não atingir a nota necessária o aluno pode permanecer no mesmo English Band por mais 2 ou 4 semanas até que consiga a evolução necessária para seguir em frente.</p>
+                <p>Uma dúvida que muitos alunos tem é sobre o número de semanas, para ter uma estimativa personalizada usamos a tabela acima como referência e os objetivos do aluno para indicar o número de semanas compatível.</p>
+                <p>Entre em contato conosco pelo WhatsApp que vamos te ajudar a identificar seu nível e quantas semanas você vai precisar </p>
+                <a href="#" class="button">Saber meu nível</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/PathwayView.vue
+++ src/views/PathwayView.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
         <pathway-component></pathway-component>
+        <pathway-levels></pathway-levels>
     </div>
 </template>
 
 <script>
-import PathwayComponent from '@/components/PathwayComponent.vue'
+import PathwayComponent from '@/components/Pathway/PathwayComponent.vue'
+import PathwayLevels from '@/components/Pathway/PathwayLevels.vue'
     export default {
-  components: { PathwayComponent },
+  components: { PathwayComponent, PathwayLevels },
         
     }
 </script>
