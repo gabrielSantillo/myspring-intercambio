3b434e20acd29fb4cae504faed69f5fb92b2883e
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-09 14:32:47 -0700

    Starting to develop the partners component

---
+++ src/components/Home/PartnersComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="partners section">
+    <div class="container"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,15 @@
   <div class="home">
     <home-component></home-component>
     <about-component></about-component>
+    <partners-component></partners-component>
   </div>
 </template>
 
 <script>
 import HomeComponent from '@/components/Home/HomeComponent.vue'
 import AboutComponent from '@/components/Home/AboutComponent.vue'
+import PartnersComponent from '@/components/Home/PartnersComponent.vue'
 export default {
-  components: { HomeComponent, AboutComponent },
+  components: { HomeComponent, AboutComponent, PartnersComponent },
 }
 </script>
