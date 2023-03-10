efb8f5a1e488d8272d11b9b8d1ac0f912ad16e5d
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-10 12:46:26 -0700

    Finishing to add the style to the agenda component

--- src/components/Agenda/AgendaComponent.vue
+++ src/components/Agenda/AgendaComponent.vue
@@ -1,15 +1,170 @@
 <template>
-    <div>
-        
+  <div class="agenda section">
+    <div class="agenda__content container">
+      <div class="agenda__content-description">
+        <h2 class="agenda__content-title">
+          Atendimento gratuito pelo WhatsApp
+        </h2>
+        <h3 class="agenda__content-subtitle">
+          Segunda a Sexta das 12:00 as 19:00 (horário de brasília)
+        </h3>
+        <p>
+          Entre em contato pelo WhatsApp para dúvidas gerais relacionadas aos:
+        </p>
+        <ul class="agenda__content-list">
+          <li class="agenda__content-list-item">
+            Programas de Online ENGLISH - ILAC
+          </li>
+          <li class="agenda__content-list-item">
+            Nivelamento e estimativas de número
+          </li>
+          <li class="agenda__content-list-item">
+            Dúvidas sobre nossos serviços de consultoria
+          </li>
+          <li class="agenda__content-list-item">
+            Orçamentos dos programas presenciais de idiomas
+          </li>
+          <li class="agenda__content-list-item">
+            Orçamentos dos programas de College Privado
+          </li>
+          <li class="agenda__content-list-item"></li>
+        </ul>
+        <p>
+          Para demais assuntos relacionados a planejamento de intercâmbio,
+          agende um horário com o consultor educaional pelo links a seguir.
+        </p>
+      </div>
+
+      <h2 class="agenda__content-title">Agendamentos</h2>
+
+      <div class="agenda__content-cards">
+        <div class="agenda__content-card">
+          <p class="agenda__content-card-title">Consulta 25 minutos</p>
+          <p class="agenda__content-card-price">CAD$ 25</p>
+          <p class="agenda__content-card-description">
+            Tire suas dúvidas sobre intercâmbio no Canadá
+          </p>
+          <a href="#" class="agenda__content-card-more">Mais informações</a>
+          <div class="line"></div>
+          <a
+            href="https://agendamentosmyspringintercambio.as.me/schedule.php?calendarID=5567721"
+            target="_blank"
+            class="agenda__content-card-button"
+            >Agendar</a
+          >
+        </div>
+
+        <div class="agenda__content-card">
+          <p class="agenda__content-card-title">Consulta 50 minutos</p>
+          <p class="agenda__content-card-price">CAD$ 50</p>
+          <p class="agenda__content-card-description">
+            Tire suas dúvidas sobre intercâmbio no Canadá: valores, linha do
+            tempo...
+          </p>
+          <a href="#" class="agenda__content-card-more">Mais informações</a>
+          <div class="line"></div>
+          <a
+            href="https://agendamentosmyspringintercambio.as.me/?calendarID=4775749"
+            target="_blank"
+            class="agenda__content-card-button"
+            >Agendar</a
+          >
+        </div>
+
+        <div class="agenda__content-card">
+          <p class="agenda__content-card-title">Consultoria de Intercâmbio</p>
+          <p class="agenda__content-card-price">CAD$ 200</p>
+          <p class="agenda__content-card-description">
+            Consultoria especializada para fins de imigração | Contrato válido para 12 meses
+          </p>
+          <a href="#" class="agenda__content-card-more">Mais informações</a>
+          <div class="line"></div>
+          <a
+            href="https://agendamentosmyspringintercambio.as.me/?calendarID=4775749"
+            target="_blank"
+            class="agenda__content-card-button"
+            >Agendar</a
+          >
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/variables.scss";
 
+.agenda__content {
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+
+.agenda__content-description {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+}
+
+.agenda__content-title {
+  text-align: center;
+  margin-bottom: 0.5rem;
+}
+
+.agenda__content-cards {
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+
+.agenda__content-card {
+  background-color: hsla(157, 16%, 92%, 0.6);
+  box-shadow: 0 2px 4px hsla(157, 54%, 18%, 0.25);
+  padding: 2rem;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.agenda__content-card-title,
+.agenda__content-card-price {
+  font-weight: $--font-semi-bold;
+  font-size: $--h2-font-size;
+}
+
+.agenda__content-card-price {
+  margin-bottom: 1rem;
+}
+
+.agenda__content-card-more {
+  margin-bottom: 1rem;
+  color: hsl(157, 64%, 18%);
+}
+
+.line {
+  width: 100%;
+  height: 0.025rem;
+  background-color: rgb(193, 214, 205);
+  margin-bottom: 1.5rem;
+}
+
+.agenda__content-card-button {
+  background-color: $--first-color;
+  color: $--white-color;
+  padding: 1rem 2rem;
+  font-weight: $--font-semi-bold;
+  transition: 0.3s;
+  border-radius: 0.5rem;
+  width: 120px;
+  height: 50px;
+  text-align: center;
+}
+
+.agenda__content-card-button:hover {
+  background-color: hsl(157, 24%, 35%);
+}
 </style>
