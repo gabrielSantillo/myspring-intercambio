5ffbc4e43b85daa061a45980966972d278e8425a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-11 12:27:48 -0700

    added the elements for the footer component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,86 @@
 <template>
-    <div>
-        
+    <div class="footer section">
+        <div class="footer__container container grid">
+            <div class="footer__content grid">
+                <div class="footer__data">
+                    <h3 class="footer__title">MySpring Intercâmbio
+                    </h3>
+                    <p class="footer__description">Nós temos a inspiração <br> e experiência para <br> te guiar nessa jornada.</p>
+
+                    <div>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-whatsapp-fill"></i></a>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-instagram-fill"></i></a>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-youtube-fill"></i></a>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-facebook-circle-fill"></i></a>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-linkedin-box-fill"></i></a>
+                        <a href="" target="_blank" class="footer__social"><i class="ri-mail-fill"></i></a>
+                        
+                    </div>
+                </div>
+
+                <div class="footer__data">
+                    <h3 class="footer__subtitle">Sobre</h3>
+
+                    <ul>
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Sobre Nós</a>
+                        </li>
+
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Consultoria</a>
+                        </li>
+
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Pathway</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer__data">
+                    <h3 class="footer__subtitle">Empresa</h3>
+
+                    <ul>
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Time</a>
+                        </li>
+
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Orçamento</a>
+                        </li>
+                        
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Agenda</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="footer__data">
+                    <h3 class="footer__subtitle">Suporte</h3>
+
+                    <ul>
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">FAQs</a>
+                        </li>
+
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Support Center</a>
+                        </li>
+                        
+                        <li class="footer__item">
+                            <a href="#" class="footer__link">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="footer__rights">
+                <p class="footer__copy">&#169; 2023 MySpring Intercâmbio. Todos os direitos reservados.</p>
+                <div class="footer__terms">
+                    <a href="#" class="footer__terms-link">Terms & Agreements</a>
+                    <a href="#" class="footer__terms-link">Política de Privacidade</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
