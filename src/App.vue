625fe5906b1e306d17020a998fd1edbc5531789a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-03-11 17:58:44 -0700

    Trying to use vuetify to use dialog boxes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <header-component></header-component>
+
     <router-view/>
     <footer-component></footer-component>
   </div>
@@ -11,6 +12,8 @@ import FooterComponent from './components/FooterComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
 export default {
   components: { HeaderComponent, FooterComponent },
+
+
   
 }
 </script>
